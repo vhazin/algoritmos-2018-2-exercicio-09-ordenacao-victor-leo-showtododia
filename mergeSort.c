67b22300d0920999e3d0df581e1fa2a7c939c766
Leonardo Castelo Branco de Melo <cbmelo97@gmail.com>
2018-11-05 00:09:08 -0300

    Update mergeSort.c
    renamed variable for better readability

--- mergeSort.c
+++ mergeSort.c
@@ -15,9 +15,9 @@ void merge(int *, int, int *, int *);
 /*** Main Function: driver program to test merge sort function ***/
 int main(void){
     int array[]  = {99,2,23,41,-5,23,-5,-10,4,8,-3}; /* Initializing random array           */
-    int elements = sizeof(array) / sizeof(array[0]); /* Finding number of elements in array */
-    mergeSort(array, elements);                      /* Merge sorting the array             */
-    for(int index = 0; index < elements; index++){   /* Printing sorted array, by element   */
+    int length = sizeof(array) / sizeof(array[0]);   /* Finding number of elements in array */
+    mergeSort(array, length);                        /* Merge sorting the array             */
+    for(int index = 0; index < length; index++){     /* Printing sorted array, by element   */
         printf("%d ", array[index]);
     }
 
@@ -25,31 +25,31 @@ int main(void){
 }
 
 /*** Merge Sort Function: Recursively sorts arrays of integers ***/
-void mergeSort(int *array, int elements) {
-	if(elements < 2) /* If number of elements = 1, return */
+void mergeSort(int *array, int length) {
+	if(length < 2) /* If number of elements = 1, return */
         return;
 
-    int mid    = elements / 2;                                   /* Find middle element */
-    int *left  = (int *)malloc((mid) * sizeof(int));             /* Allocating Memory x Number of elements in [0, ... , mid]    */
-    int *right = (int *)malloc((elements - mid) * sizeof(int));  /* Allocating Memory x Number of elements in [mid, ... , last] */
+    int mid    = length / 2;                                  /* Find middle element */
+    int *left  = (int *)malloc((mid) * sizeof(int));          /* Allocating Memory x Number of elements in [0, ... , mid]    */
+    int *right = (int *)malloc((length - mid) * sizeof(int)); /* Allocating Memory x Number of elements in [mid, ... , last] */
     
     for (int index = 0; index < mid; index++)
         left[index] = array[index];        /* Initializing `left` subarray w/ elements at indexes `array[0, ... , mid -1]`  */
-    for (int index = mid; index < elements; index++)
+    for (int index = mid; index < length; index++)
         right[index - mid] = array[index]; /* Initializing `right` subarray w/ elements at indexes `array[mid, ... , size]` */
 
-    mergeSort(left, mid);                /* Sorting `left` array: [left ... mid]       */
-    mergeSort(right, elements - mid);    /* Sorting `right` array: [mid + 1 ... right] */
-    merge(array, elements, left, right); /* Merging `left` and `right` into `array`    */
+    mergeSort(left, mid);              /* Sorting `left` array: [left ... mid]       */
+    mergeSort(right, length - mid);    /* Sorting `right` array: [mid + 1 ... right] */
+    merge(array, length, left, right); /* Merging `left` and `right` into `array`    */
     
     free(left);  /* freeing allocated memory for `left` subarray */
     free(right); /* freeing allocated memory for `right` subarray */
 }
 
 /*** Merge Function: Merges subarrays into main array ***/
-void merge(int *array, int elements, int *left, int *right) {
-    int indexLeft = 0, indexRight = 0, index = 0;              /* Initialize index for each array */
-    int lenLeft = elements / 2, lenRight = elements - lenLeft; /* Set boundries for iteration     */
+void merge(int *array, int length, int *left, int *right) {
+    int indexLeft = 0, indexRight = 0, index = 0;          /* Initialize index for each array */
+    int lenLeft = length / 2, lenRight = length - lenLeft; /* Set boundries for iteration     */
 
     /* Compare elements from subarrays index-by-index, sort, and merge */
     while(indexLeft < lenLeft && indexRight < lenRight) {
